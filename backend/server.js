6f817d4bf500a34fd456784b515380b1bd31caa0
ro-front-end <rockfrontend@outlook.com>
2025-04-01 12:55:30 -0600

    backend script

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ mongoose
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Servidor Express corriendo en el puerto ${PORT}`);
+  console.log(`Server express running on port: ${PORT}`);
 });
 
 module.exports = app;
